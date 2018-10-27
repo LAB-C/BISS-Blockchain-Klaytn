297919018b96085507a79eca1ea082e87468f145
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-10-27 20:38:57 +0900

    UPDATE : Dependencies in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# install.sh for client
+# install.sh for client(rpi)
 
-apt-get install node; npm install -g caver-js; exit 0
+apt-get --yes install node # install node 
+apt-get --yes install npm # install npm
+npm install -g caver-js
+
+apt-get --yes install arduino
+
+exit 0
